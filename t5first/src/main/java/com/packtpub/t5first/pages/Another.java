cefb02fa439f379a0637cdf12d4dcd7cdbc6c4e9
ildar shafigullin <ildar66@inbox.ru>
2016-10-03 16:30:22 +0300

    Using Page Activation Context

--- t5first/src/main/java/com/packtpub/t5first/pages/Another.java
+++ t5first/src/main/java/com/packtpub/t5first/pages/Another.java
@@ -1,37 +1,40 @@
 package com.packtpub.t5first.pages;
 
+//import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.SessionState;
 
 import com.packtpub.t5first.util.User;
+
 //import org.apache.tapestry5.ioc.services.ApplicationDefaults;
 
-
 public class Another {
-	
-	//@ApplicationDefaults
+
+	// @ApplicationDefaults
 	@SessionState
 	private User myUser;
-	
+
 	// @Persist
 	private String passedMessage;
 
 	public User getMyUser() {
 		return myUser;
 	}
-	
+
 	public String getPassedMessage() {
 		return passedMessage;
 	}
 
 	public void setPassedMessage(String passedMessage) {
+		System.out.println("setPassedMessage(String passedMessage): "
+				+ passedMessage);
 		this.passedMessage = passedMessage;
 	}
-	
+
 	void onActivate(String message) {
 		System.out.println("Another page is activated! The message is: " + message);
 		this.passedMessage = message;
 	}
-	
+
 	String onPassivate() {
 		System.out.println("Another page is passivated...");
 		return passedMessage;
