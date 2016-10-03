f316f3a5e48e45066cb40b6258075b1cbde22cf7
ildar shafigullin <ildar66@inbox.ru>
2016-10-03 15:18:59 +0300

    add Start page and make navigation to Payment.tml

---
+++ t5first/src/main/java/com/packtpub/t5first/pages/Another.java
@@ -0,0 +1,39 @@
+package com.packtpub.t5first.pages;
+
+//import org.apache.tapestry.annotations.ApplicationState;
+
+import org.apache.tapestry5.ioc.services.ApplicationDefaults;
+
+import com.packtpub.t5first.util.User;
+
+
+public class Another {
+	
+	@ApplicationDefaults
+	private User myUser;
+	
+	// @Persist
+	private String passedMessage;
+
+	public User getMyUser() {
+		return myUser;
+	}
+	
+	public String getPassedMessage() {
+		return passedMessage;
+	}
+
+	public void setPassedMessage(String passedMessage) {
+		this.passedMessage = passedMessage;
+	}
+	
+	void onActivate(String message) {
+		System.out.println("Another page is activated! The message is: " + message);
+		this.passedMessage = message;
+	}
+	
+	String onPassivate() {
+		System.out.println("Another page is passivated...");
+		return passedMessage;
+	}
+}
